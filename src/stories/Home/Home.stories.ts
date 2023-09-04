51e1f62012a89f2d1c085160673b6542fa5ed748
nostrss <jintagi@gmail.com>
2023-09-04 14:13:06 +0900

    add storybook

---
+++ src/stories/Home/Home.stories.ts
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import Home from './Home';
+
+const meta = {
+  title: 'Example/Main',
+  component: Home,
+  parameters: {
+    // More on how to position stories at: https://storybook.js.org/docs/react/configure/story-layout
+    // layout: 'fullscreen',
+  },
+} satisfies Meta<typeof Home>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const LoggedOut: Story = {};
+
+// More on interaction testing: https://storybook.js.org/docs/react/writing-tests/interaction-testing
+// export const LoggedIn: Story = {
+//   play: async ({ canvasElement }) => {
+//     const canvas = within(canvasElement);
+//     const loginButton = await canvas.getByRole('button', {
+//       name: /Log in/i,
+//     });
+//     await userEvent.click(loginButton);
+//   },
+// };
