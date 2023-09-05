19d51b2478715887812c5e4645fff11dd45eeb66
nostrss <jintagi@gmail.com>
2023-09-05 22:09:16 +0900

    postlist api

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -1,10 +1,19 @@
 import { readFile, readdir } from 'fs/promises';
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import path from 'path';
 
-export async function GET() {
+export async function GET(req: NextRequest) {
+  const url = new URL(req.url);
+
+  const page = Number(url.searchParams.get('page')) || 1;
+  const limit = Number(url.searchParams.get('limit')) || 10;
+
   const filePath = path.join(process.cwd(), 'posts');
-  const fileList = await readdir(filePath);
+  const fileList = (await readdir(filePath)).sort(function (a, b) {
+    if (a > b) return -1;
+    else if (a < b) return 1;
+    else return 0;
+  });
 
   const data = await Promise.all(
     fileList.map(async (file) => {
@@ -24,5 +33,11 @@ export async function GET() {
     })
   );
 
-  return NextResponse.json(data);
+  const sortData = data
+    .sort((a, b) => {
+      return new Date(b.date).getTime() - new Date(a.date).getTime();
+    })
+    .slice((page - 1) * limit, page * limit);
+
+  return NextResponse.json(sortData);
 }
