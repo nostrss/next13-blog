dccd64ae3ee3b22241e8226ea7c00c66cdeb5e1e
nostrss <jintagi@gmail.com>
2023-09-06 00:23:11 +0900

    post card date fix

--- src/stories/PostCard.tsx
+++ src/stories/PostCard.tsx
@@ -17,19 +17,8 @@ export default function PostCard({ post }: Props) {
       <Link href={''}>
         <article className='w-full h-full p-4 md:p-8'>
           <div className='flex items-center justify-between gap-2'>
-            <div className='w-full text-xs text-gray-800'>
-              {date ? (
-                <time dateTime={new Date(date).toISOString()}>
-                  {Intl.DateTimeFormat(undefined, {
-                    dateStyle: 'medium',
-                  }).format(new Date(date))}
-                </time>
-              ) : (
-                <span>SOON</span>
-              )}
-            </div>
+            <div className='w-full text-xs text-gray-800'>{date}</div>
           </div>
-
           <h2 className='mt-4 text-3xl font-bold text-gray-800 group-hover:text-white sm:text-4xl font-display'>
             {title}
           </h2>
