36c7fd7ad05020903441f1fdfb0aff205e74322f
nostrss <jintagi@gmail.com>
2023-09-06 14:05:23 +0900

    get blog detail data

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,3 +1,23 @@
-export default function BlogDetail() {
-  return <div>page</div>;
+import { API_BASE_URL } from '@/constant';
+
+const fetchBlogDetail = async (slug: string) => {
+  const data = await fetch(`${API_BASE_URL}/api/blog?blogid=${slug}`, {
+    method: 'GET',
+  });
+  return data.json();
+};
+
+export default async function BlogDetail({
+  params: { slug },
+}: {
+  params: { slug: string };
+}) {
+  const { data } = await fetchBlogDetail(slug);
+
+  return (
+    <article className='w-full p-3'>
+      <h1 className='mb-2 text-xl font-bold'>{data.title}</h1>
+      <p>{data.content}</p>
+    </article>
+  );
 }
