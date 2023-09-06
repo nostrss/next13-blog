d13cb1b5c958902da52a2dde02989729812a467a
nostrss <jintagi@gmail.com>
2023-09-06 13:43:28 +0900

    add Blog detail api

--- src/app/api/blog/route.ts
+++ src/app/api/blog/route.ts
@@ -1,13 +1,34 @@
+import { readFile, readdir } from 'fs/promises';
 import { NextRequest, NextResponse } from 'next/server';
+import path from 'path';
 
 export async function GET(req: NextRequest) {
   const url = new URL(req.url);
-  console.log(url.searchParams);
-  const postId = url.searchParams.get('postid');
+  const postId = url.searchParams.get('blogid');
+  const filePath = path.join(process.cwd(), 'posts');
+  const fileData = await readFile(`${filePath}/${postId}.md`, 'utf-8');
+
+  const markdowmMetaData = fileData
+    .split('---')[1]
+    .split('\n')
+    .filter(Boolean)
+    .map((item) => {
+      const [key, value] = item.split(':');
+      return { [key.trim()]: value.trim() };
+    })
+    .reduce((acc, cur) => ({ ...acc, ...cur }), {});
+
+  const markDownContent = removeMetaData(fileData);
 
   return NextResponse.json({
     data: {
-      postId,
+      ...markdowmMetaData,
+      content: markDownContent,
     },
   });
 }
+
+const removeMetaData = (markdown: string) => {
+  const regex = /^---[\s\S]*?---/m;
+  return markdown.replace(regex, '');
+};
