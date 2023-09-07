d8bd76d98e0ffb1e090127c160f10854be0ffddd
nostrss <jintagi@gmail.com>
2023-09-08 08:25:15 +0900

    giscus no emit metatag

--- src/components/CommentList.tsx
+++ src/components/CommentList.tsx
@@ -18,7 +18,7 @@ export default function CommentList() {
         data-mapping='title'
         data-strict='0'
         data-reactions-enabled='1'
-        data-emit-metadata='1'
+        data-emit-metadata='0'
         data-input-position='top'
         data-theme={theme}
         data-lang='ko'
