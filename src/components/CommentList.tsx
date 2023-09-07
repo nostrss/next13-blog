729861cdbe1d3184b68a8b1cbd406bc098c28ba6
nostrss <jintagi@gmail.com>
2023-09-08 01:24:19 +0900

    fix giscus

--- src/components/CommentList.tsx
+++ src/components/CommentList.tsx
@@ -3,10 +3,23 @@
 import Script from 'next/script';
 
 export default function CommentList() {
+  const getCookie = (cookieName: string): string => {
+    let result = '';
+    document.cookie.split(';').map((item) => {
+      const cookieItem = item.trim();
+      if (item.includes(cookieName)) {
+        result = cookieItem.split('=')[1];
+      }
+    });
+    return result;
+  };
+
+  const theme = getCookie('mode');
+
   return (
-    <div className='w-full'>
+    <div className='giscus'>
       <Script
-        className='max-w-2xl p-3'
+        className='text-center'
         src='https://giscus.app/client.js'
         data-repo='nostrss/next13-blog'
         data-repo-id={process.env.NEXT_PUBLIC_REPO_ID}
@@ -17,22 +30,12 @@ export default function CommentList() {
         data-reactions-enabled='1'
         data-emit-metadata='1'
         data-input-position='top'
-        data-theme='dark'
+        data-theme={theme}
         data-lang='ko'
         crossOrigin='anonymous'
+        data-loading='lazy'
         async
       />
     </div>
   );
 }
-
-const getCookie = (cookieName: string): string => {
-  let result = '';
-  document.cookie.split(';').map((item) => {
-    const cookieItem = item.trim();
-    if (item.includes(cookieName)) {
-      result = cookieItem.split('=')[1];
-    }
-  });
-  return result;
-};
