a8cb182c612cf94fa28f6043d16ab78c8a035885
nostrss <jintagi@gmail.com>
2023-09-13 19:12:45 +0900

    env fix

--- src/components/Analytics.tsx
+++ src/components/Analytics.tsx
@@ -2,19 +2,18 @@
 import Script from 'next/script';
 
 export default function Analytics() {
-  console.log(process.env.GA_ID);
   return (
     <>
       <Script
         async
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_ID}`}
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
         strategy='afterInteractive'
       />
       <Script id='google-analytics' strategy='afterInteractive'>
         {`window.dataLayer = window.dataLayer || []; 
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
-        gtag('config', ${process.env.GA_ID});
+        gtag('config', ${process.env.NEXT_PUBLIC_GA_ID});
           `}
       </Script>
     </>
